b8c53cb0e6479d136566c92cade2758ff0ca9dcc
Alesya <alesya.kononovich@dizzain.com>
2018-05-28 21:37:19 +0300

    homework ts

--- src/KononovichAlesya/homework.js
+++ src/KononovichAlesya/homework.js
@@ -1,4 +1,3 @@
-"use strict";
 // 1)
 // Написать функцию isInArray(), которая начиная со второго принимает переменное количество аргументов.
 //     Возвращает true, если все аргументы, кроме первого входят в первый.
@@ -21,4 +20,80 @@ var isInArray = function (sArr) {
         });
     }
     return flag;
+};
+// 2)
+// писать функцию summator(), которая сумирует переданые ей аргументы.
+//     Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
+var summator = function () {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
+    }
+    var sum = 0;
+    rest.forEach(function (item) {
+        if (typeof item === 'number') {
+            sum += item;
+        }
+        else {
+            sum += parseFloat(item);
+        }
+    });
+    return sum;
+};
+// 3)
+// Написать функцию getUnique(arr), которая принимает аргументом неограниченое число аргументов,
+//     и возвращает массив уникальных элементов. Аргумент не должен изменяться.
+//     Порядок элементов результирующего массива должен совпадать с порядком,
+//     в котором они встречаются в оригинальной структуре.
+var getUnique = function () {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
+    }
+    var newArr = [];
+    rest.forEach(function (item) {
+        if (newArr.indexOf(item) === -1) {
+            newArr.push(item);
+        }
+    });
+    return newArr;
+};
+// 4)
+// Написать функцию котороя будет разворачивать буквы в словах предложения, но только лишь буквы
+// цифры и специальные символы должны остаться на месте
+// s1tar3t 2 hellow  ->  t1rat3s 2 wolleh
+// s1ta$%r3t 2 hel^low  ->  t1ra$%t3s 2 wol^leh
+// s1tar3t 2   low5  ->  t1rat3s 2   wol5
+var reverseSentence = function (str) {
+    var res = '';
+    var strArr = str.split(' ');
+    var newWorlds = [];
+    if (strArr.length) {
+        strArr.forEach(function (item) {
+            var word = item;
+            var reverseWord = '';
+            var letters = [];
+            var i = 0;
+            for (var _i = 0, word_1 = word; _i < word_1.length; _i++) {
+                var chr = word_1[_i];
+                if (chr.match(/[a-zа-яё]/i)) {
+                    letters.push(chr);
+                }
+            }
+            letters = letters.reverse();
+            for (var _a = 0, word_2 = word; _a < word_2.length; _a++) {
+                var chr = word_2[_a];
+                if (chr.match(/[a-zа-яё]/i)) {
+                    reverseWord += letters[i];
+                    i++;
+                }
+                else {
+                    reverseWord += chr;
+                }
+            }
+            newWorlds.push(reverseWord);
+        });
+    }
+    res = newWorlds.join(' ');
+    return res;
 };
--- src/KononovichAlesya/homework.ts
+++ src/KononovichAlesya/homework.ts
@@ -3,18 +3,91 @@
 //     Возвращает true, если все аргументы, кроме первого входят в первый.
 //     Первым всегда должен быть массив.
 
-    let isInArray: ( sArr: Array<string | symbol | number>, ...rest: any[] ) => boolean =
-    ( sArr: Array<string | symbol | number>, ...rest: any[] ) => {
-        let flag: boolean = true;
-        if ( !Array.isArray( sArr ) || !rest.length ) {
-            flag = false;
-        } else {
-            rest.forEach(( item ) => {
-               if ( sArr.indexOf( item ) === -1 ) {
-                   flag = false;
-                   return;
-               }
-            });
-        }
-        return flag;
-    };
+    let isInArray: ( sArr: Array<string | number>, ...rest: any[] ) => boolean =
+        ( sArr: Array<string | symbol | number>, ...rest: any[] ) => {
+            let flag: boolean = true;
+            if ( !Array.isArray( sArr ) || !rest.length ) {
+                flag = false;
+            } else {
+                rest.forEach(( item ) => {
+                   if ( sArr.indexOf( item ) === -1 ) {
+                       flag = false;
+                       return;
+                   }
+                });
+            }
+            return flag;
+        };
+
+// 2)
+// писать функцию summator(), которая сумирует переданые ей аргументы.
+//     Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
+
+    let summator: (...rest: Array<string | number> ) => number =
+        ( ...rest ) => {
+            let sum: number = 0;
+            rest.forEach( ( item ) => {
+                if ( typeof item === 'number' ) {
+                    sum += item;
+                } else {
+                    sum += parseFloat( item );
+                }
+            } );
+            return sum;
+        };
+
+// 3)
+// Написать функцию getUnique(arr), которая принимает аргументом неограниченое число аргументов,
+//     и возвращает массив уникальных элементов. Аргумент не должен изменяться.
+//     Порядок элементов результирующего массива должен совпадать с порядком,
+//     в котором они встречаются в оригинальной структуре.
+
+    let getUnique: ( ...rest: any[] ) => any[] =
+        ( ...rest ) => {
+            const newArr: any[] = [];
+            rest.forEach( ( item ) => {
+                if ( newArr.indexOf( item ) === -1 ) {
+                    newArr.push( item );
+                }
+            } );
+            return newArr;
+        };
+
+// 4)
+// Написать функцию котороя будет разворачивать буквы в словах предложения, но только лишь буквы
+// цифры и специальные символы должны остаться на месте
+// s1tar3t 2 hellow  ->  t1rat3s 2 wolleh
+// s1ta$%r3t 2 hel^low  ->  t1ra$%t3s 2 wol^leh
+// s1tar3t 2   low5  ->  t1rat3s 2   wol5
+
+    let reverseSentence: ( str: string ) => string =
+        ( str: string ) => {
+            let res: string = '';
+            const strArr: Array<string> = str.split( ' ' );
+            const newWorlds: Array<string> = [];
+            if ( strArr.length ) {
+               strArr.forEach( ( item ) => {
+                    const word = item;
+                    let reverseWord: string = '';
+                    let letters: Array<string> = [];
+                    let i = 0;
+                    for (const chr of word ) {
+                        if ( chr.match(/[a-zа-яё]/i ) ) {
+                            letters.push( chr );
+                        }
+                    }
+                    letters = letters.reverse();
+                    for (const chr of word ) {
+                        if ( chr.match(/[a-zа-яё]/i ) ) {
+                            reverseWord += letters[i];
+                            i++;
+                        } else {
+                            reverseWord += chr;
+                        }
+                    }
+                    newWorlds.push( reverseWord );
+               });
+            }
+            res = newWorlds.join( ' ' );
+            return res;
+        };
